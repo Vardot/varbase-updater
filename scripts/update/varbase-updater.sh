5fc115c30be2ff92d44bd46ac0711ef985d7cd13
Rajab Natshah <rajabn@gmail.com>
2019-12-21 16:06:01 +0200

    Fix update-vabase.sh still invokes drush entup #21 : by horvan

--- scripts/update/varbase-updater.sh
+++ scripts/update/varbase-updater.sh
@@ -280,7 +280,6 @@ elif [ "$answer" != "${answer#[YyUu]}" ] ; then
   echo -e "$(tput setaf 2)Updating the database for latest changes.$(tput sgr 0)";
   echo -e "$(tput setaf 2)Updating the database for latest changes.$(tput sgr 0)" >> ${ERRORLOG};
   $DRUSH  cache-rebuild --yes;
-  $DRUSH  entity-updates --yes;
   $DRUSH  updb --yes --strict=0 1> >(tee -a ${ERRORLOG} >&1) 2> >(tee -a ${ERRORLOG} >&2);
   result="$?";
   if [ "$result" -ne 0 ]; then
