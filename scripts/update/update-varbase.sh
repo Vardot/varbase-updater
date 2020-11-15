2a0333598b61693b2d9e60279d0409ae0682cf37
Rajab Natshah <rajabn@gmail.com>
2020-11-15 18:49:55 +0200

    Change composer self-update to composer self-update --1 to work with composer ~1.0 #34

--- scripts/update/update-varbase.sh
+++ scripts/update/update-varbase.sh
@@ -5,7 +5,7 @@
 BASEDIR=$(pwd);
 
 echo "$(tput setaf 2)Checking composer version and updating if needed:$(tput sgr 0)";
-composer self-update;
+composer self-update --1;
 echo "$(tput setaf 2)Checking varbase-updater version and updating if needed:$(tput sgr 0)";
 composer update vardot/varbase-updater;
 
